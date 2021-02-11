4cb0ff8fcc650bf81a380c27d1eb443c186534b5
uthircloudnative <uthircloudnative@gmail.com>
2021-02-11 16:39:53 -0500

    Initial commit

---
+++ src/main/java/com/cicd/herokuautodeploy/HerokuautodeployApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.herokuautodeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuautodeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuautodeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cicd/herokuautodeploy/HerokuautodeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.herokuautodeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuautodeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
